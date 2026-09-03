6c4ac666e6de0dbbbd707d64b698ba0bb80d2148
fayechiang <wek6mq@virginia.edu>
2026-09-03 10:39:21 -0400

    Update python_practice.py
    Added name.

--- python_practice.py
+++ python_practice.py
@@ -1,6 +1,7 @@
 # This is your first coding assignment for Computational BME.
 # As discussed in class, feel free to use AI tools to help you complete this assignment, but remember to cite them.
-# I encourage you to try the problems yourself first and only use AI tools when you are stuck to benefit your learning. 
+# I encourage you to try the problems yourself first and only use AI tools when you are stuck to benefit your learning.
+# Name: Faye Chiang 
 
 # %% ###########################################################
 # Problem 1: Practice writing pseudocode
